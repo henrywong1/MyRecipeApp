ec13ba4fdf94f163bd7be532ea55bcad2c4b8cfd
Henry Wong <H_wong14@u.pacific.edu>
2019-07-26 15:01:39 -0700

    Added hidden API key, Added Search using Stringbuilder instead of String, Logs out ten results

--- app/src/main/java/com/example/henry/myrecipeapp/MainActivity.java
+++ app/src/main/java/com/example/henry/myrecipeapp/MainActivity.java
@@ -3,6 +3,14 @@ package com.example.henry.myrecipeapp;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -11,33 +19,40 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
+    EditText searchEditText;
+
+    private static String API_ID = BuildConfig.ApiID;
+    private static String API_KEY = BuildConfig.ApiKEY;
+
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
         protected String doInBackground(String... urls) {
+                StringBuilder stringBuilder = new StringBuilder();
+                String result = " ";
+                URL url;
+                HttpURLConnection urlConnection = null;
 
-            String result = " ";
-            URL url;
-            HttpURLConnection urlConnection = null;
+                try{
+                    url = new URL(urls[0]);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader reader = new InputStreamReader(in);
 
-            try{
-                url = new URL(urls[0]);
-                urlConnection = (HttpURLConnection) url.openConnection();
-                InputStream in = urlConnection.getInputStream();
-                InputStreamReader reader = new InputStreamReader(in);
+                    int data = reader.read();
 
-                int data = reader.read();
+                    while (data != -1) {
+                        char curr  = (char) data;
+                        stringBuilder.append(curr);
+                        data = reader.read();
+                    }
+                    result = stringBuilder.toString();
+                    return result;
 
-                while (data != -1) {
-                    char curr  = (char) data;
-                    result += curr;
-                    data = reader.read();
-                }
-                return result;
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 return null;
             }
 
@@ -46,9 +61,39 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(s);
+                String hits = jsonObject.getString("hits");
+
+                JSONArray arr = new JSONArray(hits);
+                JSONObject jsonPart;
+                JSONObject jsonLabel;
+                for (int i = 0; i < 10; i++){
+                    jsonPart = arr.getJSONObject(i);
+                    jsonLabel = jsonPart.getJSONObject("recipe");
+                    Log.i("Title", jsonLabel.getString("label"));
+
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
         }
     }
 
+    public void searchRecipe(View view) {
+        Log.i("Button","Pressed");
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://api.edamam.com/search?q=" + searchEditText.getText() + "&app_id=$" + API_ID + "&app_key=$" + API_KEY);
+        }catch (Exception e) {
+            Toast.makeText(this,"Fail", Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
+    }
 
 
 
@@ -57,6 +102,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = findViewById(R.id.searchButton);
+        searchEditText = findViewById(R.id.searchEditText);
 
 
     }
