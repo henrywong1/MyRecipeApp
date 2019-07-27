b4d243e0b2e1137c692a408be4e54ab3d0ca7edf
Henry Wong <H_wong14@u.pacific.edu>
2019-07-26 22:44:19 -0700

    Added ability to click on listview to get a webview of recipe direction

--- app/src/main/java/com/example/henry/myrecipeapp/listActivity.java
+++ app/src/main/java/com/example/henry/myrecipeapp/listActivity.java
@@ -1,16 +1,17 @@
 package com.example.henry.myrecipeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class listActivity extends AppCompatActivity {
 
     ListView listView;
-
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,13 @@ public class listActivity extends AppCompatActivity {
 
         listView.setAdapter(arrayAdapter);
 
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getApplicationContext(), webActivity.class);
+                intent.putExtra("url", MainActivity.recipeURL.get(i));
+                startActivity(intent);
+            }
+        });
     }
 }
